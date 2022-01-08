3c41499f1752b92fa32e8075562a0197e2dd24f2
叶继伟 <729721503@qq.com>
2022-01-08 21:25:55 +0800

    fix: Topnav样式修改

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="topnav">
-    <div class="ui-logo">
+    <a class="ui-logo" href="/home">
       <img src="" alt="" />
       <span>JW UI</span>
-    </div>
+    </a>
     <div class="right-wrapper">
-      <router-link to="/">首页</router-link>
+      <router-link exact to="/home">首页</router-link>
       <router-link to="/doc">文档</router-link>
+      <a href="https://github.com/coderyjw/jw-ui" target="_blank">GitHub</a>
+      <span>1.0.0</span>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .topnav {
-  height: 77px;
+  height: 63px;
   background-color: #fff;
   padding: 0 30px;
   display: flex;
@@ -27,6 +29,34 @@
       cursor: pointer;
       font-size: 18px;
       line-height: 28.8px;
+    }
+    &:hover {
+      border-bottom: none;
+    }
+  }
+
+  .right-wrapper {
+    a {
+      display: inline-block;
+      text-decoration: none;
+      padding: 0 20px;
+      height: 63px;
+      line-height: 63px;
+
+      &:hover,
+      &:active {
+        color: rgb(24, 160, 88);
+      }
+    }
+
+    .router-link-active {
+      color: rgb(24, 160, 88);
+      border-bottom: 1px solid;
+    }
+
+    span {
+      color: rgb(51, 54, 57);
+      font-size: 14px;
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "@/assets/css/index.css";
+import "@/assets/css/index.scss";
 
 const app = createApp(App);
 app.use(router);
