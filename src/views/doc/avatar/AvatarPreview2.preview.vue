22d5a99dde3b81041969fd3583d924d3f67c397f
叶继伟 <729721503@qq.com>
2022-01-16 08:21:29 +0800

    fix: Avatar图片打包后不显示的问题

--- src/views/doc/avatar/AvatarPreview2.preview.vue
+++ src/views/doc/avatar/AvatarPreview2.preview.vue
@@ -1,7 +1,10 @@
 <preview>形状</preview>
 <template>
-  <jw-avatar src="/src/assets/image/avator.png" shape="square" size="small" />
-  <jw-avatar src="/src/assets/image/avator.png" shape="square" size="default" />
-  <jw-avatar src="/src/assets/image/avator.png" shape="square" size="large" />
-  <jw-avatar src="/src/assets/image/avator.png" shape="square" :size="100" />
+  <jw-avatar :src="imgUrl" shape="square" size="small" />
+  <jw-avatar :src="imgUrl" shape="square" size="default" />
+  <jw-avatar :src="imgUrl" shape="square" size="large" />
+  <jw-avatar :src="imgUrl" shape="square" :size="100" />
 </template>
+<script setup lang="ts">
+import imgUrl from "/src/assets/image/avator.png";
+</script>
