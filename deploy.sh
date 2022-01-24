6e6b3b88a5b71538daa3ad3056a577280d4b7ae7
叶继伟 <729721503@qq.com>
2022-01-24 18:56:27 +0800

    fix: 一键部署整合

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,18 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
+git remote add origin git@gitee.com:coderyjw/jw-ui-website.git &&
+git push -f -u origin master &&
+
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
 git remote add origin git@github.com:coderyjw/jw-ui-website.git &&
 git push -f -u origin master &&
+
 cd -
 echo https://coderyjw.github.io/jw-ui-website/
