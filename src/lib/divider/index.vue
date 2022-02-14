c3e73cd08ddc5228f90cbc220c77dd293447ba54
yjw <yejw6@asiainfo.com>
2022-02-14 15:00:10 +0800

    feat: Divider 基础用法

--- src/lib/divider/index.vue
+++ src/lib/divider/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="jw-divider">divider</div>
+  <div class="jw-divider">
+    <div class="jw-divider-line"></div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
@@ -11,5 +13,18 @@ export default {
 
 <style lang="scss">
 .jw-divider {
+  margin: 24px 0;
+  position: relative;
+  display: flex;
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 16px;
+  color: rgb(31, 34, 37);
+  .jw-divider-line {
+    background-color: rgb(239, 239, 245);
+    height: 1px;
+    border: none;
+    width: 100%;
+  }
 }
 </style>
--- src/views/doc/divider/DividerPreview1.preview.vue
+++ src/views/doc/divider/DividerPreview1.preview.vue
@@ -1,6 +1,8 @@
 <preview>基础用法</preview>
 <template>
+  啊
   <jw-divider />
+  哈
 </template>
 
 <script setup lang="ts"></script>
