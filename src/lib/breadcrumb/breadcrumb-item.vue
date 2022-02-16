47041bceda1299bb6b4f8680e91818ca94ae9b8e
yjw <yejw6@asiainfo.com>
2022-02-16 16:04:43 +0800

    feat: Breadcrumb基础使用

--- src/lib/breadcrumb/breadcrumb-item.vue
+++ src/lib/breadcrumb/breadcrumb-item.vue
@@ -1,10 +1,46 @@
 <template>
-  <div class="jw-breadcrumb-item">
-    <slot></slot>
-  </div>
+  <span class="jw-breadcrumb-item">
+    <span
+      class="jw-breadcrumb-inner"
+      :class="{
+        'is-link': to,
+      }"
+      @click="handleClick"
+    >
+      <slot></slot>
+    </span>
+    <jw-icon
+      v-if="parent?.separatorIcon"
+      class="jw-breadcrumb-separator"
+      :size="12"
+    >
+      <component :is="parent.separatorIcon" />
+    </jw-icon>
+    <span v-else class="jw-breadcrumb-separator">{{ parent.separator }}</span>
+  </span>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject } from "vue";
+import { useRouter } from "vue-router";
+const props = defineProps({
+  to: {
+    type: Object,
+    default: () => {},
+  },
+  replace: {
+    type: Boolean,
+    default: false,
+  },
+});
+const router = useRouter();
+const parent = inject("JwBreadcrumbKey", undefined);
+
+const handleClick = () => {
+  if (!props.to || !router) return;
+  props.replace ? router.replace(props.to) : router.push(props.to);
+};
+</script>
 <script lang="ts">
 export default {
   name: "JwBreadcrumbItem",
@@ -12,5 +48,31 @@ export default {
 </script>
 <style lang="scss">
 .jw-breadcrumb-item {
+  float: left;
+  display: flex;
+  align-items: center;
+  color: #606266;
+  .jw-breadcrumb-separator {
+    margin: 0 9px;
+    font-weight: 700;
+    color: #c0c4cc;
+  }
+
+  .jw-breadcrumb-inner.is-link,
+  .jw-breadcrumb-inner a {
+    font-weight: 700;
+    text-decoration: none;
+    color: #303133;
+    transition: color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+    cursor: pointer;
+
+    &:hover {
+      color: #36ad6a;
+    }
+  }
+
+  &:last-child .jw-breadcrumb-separator {
+    display: none;
+  }
 }
 </style>
--- src/lib/breadcrumb/breadcrumb.vue
+++ src/lib/breadcrumb/breadcrumb.vue
@@ -4,7 +4,19 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { provide } from "vue";
+const props = defineProps({
+  separator: {
+    type: String,
+    default: "/",
+  },
+  separatorIcon: {
+    type: Object,
+  },
+});
+provide("JwBreadcrumbKey", props);
+</script>
 <script lang="ts">
 export default {
   name: "JwBreadcrumb",
@@ -13,5 +25,9 @@ export default {
 
 <style lang="scss">
 .jw-breadcrumb {
+  display: flex;
+  width: 100%;
+  font-size: 14px;
+  line-height: 1;
 }
 </style>
