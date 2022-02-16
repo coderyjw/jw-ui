59441748bf06ea5b885222ce1375521066a2db5d
yjw <yejw6@asiainfo.com>
2022-02-16 10:39:28 +0800

    feat: 固定 Affix 组件的位置

--- src/views/doc/affix/AffixPreview1.preview.vue
+++ src/views/doc/affix/AffixPreview1.preview.vue
@@ -3,7 +3,4 @@
   <jw-affix :offset="120" position="top">
     <jw-button>距离顶部120px固定</jw-button>
   </jw-affix>
-  <jw-affix :offset="120" position="bottom">
-    <jw-button>距离底部120px固定</jw-button>
-  </jw-affix>
 </template>
---
+++ src/views/doc/affix/AffixPreview3.preview.vue
@@ -0,0 +1,6 @@
+<preview>固定 Affix 组件的位置</preview>
+<template>
+  <jw-affix :offset="120" position="bottom">
+    <jw-button>距离底部120px固定</jw-button>
+  </jw-affix>
+</template>
