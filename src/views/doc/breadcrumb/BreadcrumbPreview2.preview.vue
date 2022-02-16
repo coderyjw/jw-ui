f2289fadf703b366fd482619d9dd486b30f85fe7
yjw <yejw6@asiainfo.com>
2022-02-16 16:09:00 +0800

    feat: Breadcrumb 图标分隔符

--- src/views/doc/breadcrumb/BreadcrumbPreview2.preview.vue
+++ src/views/doc/breadcrumb/BreadcrumbPreview2.preview.vue
@@ -1,6 +1,6 @@
-<preview>基础用法</preview>
+<preview>图标分隔符</preview>
 <template>
-  <jw-breadcrumb separator="/">
+  <jw-breadcrumb :separator-icon="ArrowForwardIosSharp">
     <jw-breadcrumb-item :to="{ path: '/' }">homepage</jw-breadcrumb-item>
     <jw-breadcrumb-item
       ><a href="/">promotion management</a></jw-breadcrumb-item
@@ -9,3 +9,7 @@
     <jw-breadcrumb-item>promotion detail</jw-breadcrumb-item>
   </jw-breadcrumb>
 </template>
+
+<script setup lang="ts">
+import { ArrowForwardIosSharp } from "@vicons/material";
+</script>
--- src/views/doc/breadcrumb/index.vue
+++ src/views/doc/breadcrumb/index.vue
@@ -3,6 +3,7 @@
     <h1>面包屑 Breadcrumb</h1>
     <div class="preview-wrapper">
       <Preview :component="BreadcrumbPreview1" />
+      <Preview :component="BreadcrumbPreview2" />
     </div>
   </div>
 </template>
@@ -10,6 +11,7 @@
 <script setup lang="ts">
 import Preview from "@/components/Preview.vue";
 import BreadcrumbPreview1 from "./BreadcrumbPreview1.preview.vue";
+import BreadcrumbPreview2 from "./BreadcrumbPreview2.preview.vue";
 </script>
 
 <style scoped></style>
